fae0e630177e54e5b2476c434cca51e4ee7c3d63
Pietro Agnoli <87072344+TheCaptainCraken@users.noreply.github.com>
2024-01-09 12:57:13 +0100

    new shader

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEGIHT 900
 
-#define SHADER_NAME "resources/solid_color.frag"
+#define SHADER_NAME "resources/trippy_image.frag"
 
 int main(void) {
   InitWindow(SCREEN_WIDTH, SCREEN_HEGIHT, "Fractal Voyager");
@@ -15,18 +15,30 @@ int main(void) {
 
   Shader shader = LoadShader(NULL, SHADER_NAME);
 
+  int shader_resolution_location = GetShaderLocation(shader, "resolution");
+  float resolution[2] = {(float)SCREEN_WIDTH, (float)SCREEN_HEGIHT};
+  SetShaderValue(shader, shader_resolution_location, resolution,
+                 SHADER_UNIFORM_VEC2);
+
+  int shader_time_location = GetShaderLocation(shader, "time");
+  float time = 0.0;
+
   SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
+
+    time = (float)GetTime();
+    SetShaderValue(shader, shader_time_location, &time, SHADER_UNIFORM_FLOAT);
+
     BeginDrawing();
     ClearBackground(RAYWHITE);
 
     BeginShaderMode(shader);
 
     DrawTexture(texture, 0, 0, WHITE);
-    
+
     EndShaderMode();
-    
+
     EndDrawing();
   }
 
